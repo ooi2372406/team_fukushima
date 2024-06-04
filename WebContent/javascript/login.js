c76d94720b172033af65fa6cca9d76413b88c789
ooi2372406 <ooi2372406@stu.o-hara.ac.jp>
2024-06-04 22:24:50 +0900

    ログイン画面sample

---
+++ WebContent/javascript/login.js
@@ -0,0 +1,8 @@
+document.getElementById('show-password').addEventListener('change', function() {
+    var passwordInput = document.getElementById('password');
+    if (this.checked) {
+        passwordInput.type = 'text';
+    } else {
+        passwordInput.type = 'password';
+    }
+});
