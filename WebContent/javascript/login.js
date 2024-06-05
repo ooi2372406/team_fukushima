e6e2c0cfd81ae7ec1d53ad9accdb9da14e6c14b3
ooi2372406 <ooi2372406@stu.o-hara.ac.jp>
2024-06-05 10:25:01 +0900

    header footer 作成

--- WebContent/javascript/login.js
+++ WebContent/javascript/login.js
@@ -6,3 +6,16 @@ document.getElementById('show-password').addEventListener('change', function() {
         passwordInput.type = 'password';
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+	var input = document.querySelector('.exampleInputEmail1');
+    var formGroup = document.querySelector('.form-group');
+
+    input.addEventListener('focus', function() {
+        formGroup.classList.add('highlight');
+    });
+
+    input.addEventListener('blur', function() {
+        formGroup.classList.remove('highlight');
+    });
+});
