2ec9658a8d02440a9d0c3325ba9125e674625cfb
@Fukushima797 <ooi2372125@stu.o-hara.ac.jp>
2024-06-10 13:50:18 +0900

    TestListStudentとTestListSubjectを追記

---
+++ src/bean/TestListSubject.java
@@ -0,0 +1,65 @@
+package bean;
+
+import java.util.Map;
+
+public class TestListSubject {
+
+	private int entYear;
+	private String studentNo;
+	private String studentName;
+	private String classNum;
+	private Map<Integer,Integer> points;
+
+	public TestListSubject(){
+
+	}
+
+	public int getEntYear(){
+		return entYear;
+	}
+
+	public String getStudentNo(){
+		return studentNo;
+	}
+
+	public String getStudentName(){
+		return studentName;
+	}
+
+	public String getClassNum(){
+		return classNum;
+	}
+
+	public Map<Integer,Integer> getPoints(){
+		return points;
+	}
+
+	public String getPoint(int key){
+		//とりあえずnull
+		 return null;
+	}
+
+	public void setEntYear(int entYear){
+		this.entYear=entYear;
+	}
+
+	public void setStudentNo(String studentNo){
+		this.studentNo=studentNo;
+	}
+
+	public void setStudentName(String studentName){
+		this.studentName=studentName;
+	}
+
+	public void setClassNum(String classNum){
+		this.classNum=classNum;
+	}
+
+	public void setPoints(Map<Integer,Integer> points){
+		this.points=points;
+	}
+
+	public void putPoint(int key,int value){
+		//ここがよくわからない
+	}
+}
