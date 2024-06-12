645a4d22dd7974fe9d006700f30a0e24067ba517
@Fukushima797 <ooi2372125@stu.o-hara.ac.jp>
2024-06-12 15:04:30 +0900

    参照機能のDAOの追記

--- src/student/TestListAction.java
+++ src/student/TestListAction.java
@@ -6,14 +6,20 @@ import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import bean.School;
 import bean.Subject;
+import bean.Teacher;
 import bean.TestListStudent;
 import bean.TestListSubject;
+import dao.ClassNumDao;
+import dao.SubjectDAO;
 import dao.TestListStudentDAO;
 import dao.TestListSubjectDAO;
 import tool.Action;
+import util.Util;
+
 public class TestListAction extends Action {
 
     // DAOインスタンスの作成
@@ -26,52 +32,60 @@ public class TestListAction extends Action {
 
         // actionに応じた処理の実行
         try {
-            if ("getStudents".equals(action)) {
-                setTestListStudent(req, res);
-            } else if ("getSubjects".equals(action)) {
-                setTestListSubject(req, res);
-            } else {
-                res.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid action");
-            }
+        	HttpSession session = req.getSession();
+        	Teacher teacher=Util.getUser(req);
+        	School school=teacher.getSchool();
+
+        	SubjectDAO subject_dao=new SubjectDAO();
+        	List<Subject>subject = subject_dao.get(school);
+
+        	ClassNumDao class_dao = new ClassNumDao();
+        	List<String>classnum=class_dao.Filter(school);
+
+        	session.setAttribute("subject", subject);
+        	session.setAttribute("classnum", classnum);
+
+        	return "test_list.jsp";
+
         } catch (Exception e) {
             e.printStackTrace();
             res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error processing request");
         }
-        return "test_list.jsp";
+
     }
 
-    private void setTestListStudent(HttpServletRequest req, HttpServletResponse res) throws Exception {
+private void setTestListStudent(HttpServletRequest req, HttpServletResponse res) throws Exception {
         // リクエストパラメータの取得
-        String cid = req.getParameter("cid");
+        String cd = req.getParameter("cd");
         String subject = req.getParameter("subject");
 
         // 学生情報の取得
         TestListStudent filterStudent = new TestListStudent();
-        filterStudent.setSubjectCd(cid);
+        filterStudent.setSubjectCd(cd);
         filterStudent.setSubjectName(subject);
 
-        List<TestListStudent> studentList = studentDao.postFilter(filterStudent);
+
 
         // 結果をリクエスト属性に設定して、JSPに転送
-        req.setAttribute("studentList", studentList);
+        req.setAttribute("studentList", filterStudent);
         RequestDispatcher dispatcher = req.getRequestDispatcher("/test_list.jsp");
         dispatcher.forward(req, res);
     }
 
-    private void setTestListSubject(HttpServletRequest req, HttpServletResponse res) throws Exception {
+public void setTestListSubject(HttpServletRequest req, HttpServletResponse res) throws Exception {
         // リクエストパラメータの取得
         int entYear = Integer.parseInt(req.getParameter("entYear"));
         String classNum = req.getParameter("classNum");
-        String subjectCid = req.getParameter("subjectCid");
-        String schoolCid = req.getParameter("schoolCid");
+        String subjectCd = req.getParameter("subjectCd");
+        String schoolCd = req.getParameter("schoolCd");
 
         // 科目情報の取得
         Subject subject = new Subject();
-        subject.setCd(subjectCid);
+        subject.setCd(subjectCd);
         School school = new School();
-        school.setCd(schoolCid);
+        school.setCd(schoolCd);
 
-        List<TestListSubject> subjectList = subjectDao.filter(entYear, classNum, subject, school);
+        List<TestListSubject> subjectList = subjectDao.filter(entYear, classNum, subjectCd, school);
 
         // 結果をリクエスト属性に設定して、JSPに転送
         req.setAttribute("subjectList", subjectList);
